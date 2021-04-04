ec7608519ff9e11f71c11c4044a553b8603d3f45
Richard Diamond <dick@richardcdiamond.com>
2021-04-04 12:56:38 -0700

    Fix nix-build.

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -1,28 +1,25 @@
-self: pkgs:
-
-with pkgs;
-
-rec {
-  geobacterLlvmPackages_latest = callPackage ./development/compilers/geobacter-rust/llvm ({
-    inherit (stdenvAdapters) overrideCC;
-    buildLlvmTools = buildPackages.llvmPackages_11.tools;
-    targetLlvmLibraries = targetPackages.llvmPackages_11.libraries;
-  } // lib.optionalAttrs (stdenv.hostPlatform.isi686 && buildPackages.stdenv.cc.isGNU) {
-    stdenv = gcc7Stdenv;
+self: super:
+{
+  geobacterLlvmPackages_latest = self.callPackage ./development/compilers/geobacter-rust/llvm ({
+    inherit (self.stdenvAdapters) overrideCC;
+    buildLlvmTools = self.buildPackages.llvmPackages_11.tools;
+    targetLlvmLibraries = self.targetPackages.llvmPackages_11.libraries;
+  } // self.lib.optionalAttrs (self.stdenv.hostPlatform.isi686 && self.buildPackages.stdenv.cc.isGNU) {
+    stdenv = self.gcc7Stdenv;
   });
-  geobacterLlvm_latest = geobacterLlvmPackages_latest.llvm;
+  geobacterLlvm_latest = self.geobacterLlvmPackages_latest.llvm;
   # The same LLVM that Geobacter uses, so you can install the LLVM tools w/o rebuilding.
-  geobacterRustcLlvmPackages = geobacterLlvm_latest.override {
+  geobacterRustcLlvmPackages = self.geobacterLlvm_latest.override {
     enableSharedLibraries = false;
     enablePolly = true;
     buildType = "Release";
     enableAssertions = true;
   };
-  geobacterRust_1_49 = callPackage ./development/compilers/geobacter-rust/latest.nix {
-    inherit (darwin.apple_sdk.frameworks) CoreFoundation Security;
+  geobacterRust_1_49 = self.callPackage ./development/compilers/geobacter-rust/latest.nix {
+    inherit (self.darwin.apple_sdk.frameworks) CoreFoundation Security;
   };
-  geobacterRust = geobacterRust_1_49;
-  geobacterRustPackages = geobacterRust_1_49.packages.stable;
-  geobacterRustPlatform = geobacterRustPackages.rustPlatform;
-  makeGeobacterRustPlatform = callPackage ./development/compilers/geobacter-rust/make-rust-platform.nix {};
+  geobacterRust = self.geobacterRust_1_49;
+  geobacterRustPackages = self.geobacterRust_1_49.packages.stable;
+  geobacterRustPlatform = self.geobacterRustPackages.rustPlatform;
+  makeGeobacterRustPlatform = self.callPackage ./development/compilers/geobacter-rust/make-rust-platform.nix {};
 }
